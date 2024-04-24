c3fabc3f468d7aa340e88b909fa135ecab7ad8a3
sh <sandrahammamitlili@gmail.com>
2024-04-24 12:31:42 +0200

    commit-validation-mail

--- backend-mongoose-atelier/routes/user.route.js
+++ backend-mongoose-atelier/routes/user.route.js
@@ -4,6 +4,21 @@ const User = require('../models/user.js');
 const  jwt  = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
+const UserActivation = require('../models/userActivation.js');
+// bibliothèque nodemailer
+
+const nodemailer=require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.ethereal.email',
+  port: 587,
+  secure: false, // Use `true` for port 465, `false` for all other ports
+  auth: {
+      user: 'ozella.zieme@ethereal.email',
+      pass: 'E3yyaRmyPUZwbFVy5v'
+  }
+});
+
 //Register
 
 router.post('/register', async (req, res, )=> {
@@ -26,12 +41,100 @@ router.post('/register', async (req, res, )=> {
     try {
            await newUser.save();
    
+// Début traitement node mailer
+/*
+*************************************************************** 
+*/
+
+// Save dans userActivation
+
+const newUserActivation=new UserActivation({
+  isActive:false,
+  userID : newUser._id
+    });
+
+try {
+       await newUserActivation.save();
+    }
+catch (err) {console.log(err)}
+
+// Envoyer l'e-mail de confirmation de l'inscription
+var mailOption ={
+  from: '"verify your email " <abcCorporation@gmail.com>',
+  to: newUser.email,
+  subject: 'Verification of your email ',
+  html:`<h2>${newUser.name}! Thank you for registreting on our website</h2>
+  <h4>Please verify your email to procced... </h4>
+  <a href="http://${req.headers.host}/api/users/status/edit?email=${newUser.email}">click here</a>`
+}
+transporter.sendMail(mailOption,function(error,info){
+  if(error){
+    console.log(error)
+  }
+  else{
+    console.log('verification email sent to your gmail account ')
+  }
+})
+
+/*
+*************************************************************** 
+*/
+// fin traitement node mailer
+
+
         return res.status(201).send({ success: true, message: "Account created successfully", user: newUser })
        } catch (error) {
            res.status(409).json({ message: error.message });
        }
   
 });
+
+
+// Début suite traitement node mailer
+
+/*
+*************************************************************** 
+*/
+
+
+router.get('/status/edit', async (req, res) => {
+  try {
+  let email = req.query.email
+  let user = await User.findOne({email})
+  
+  let userActivated = await UserActivation.findOne({userID :user._id})
+  
+  userActivated.isActive = !userActivated.isActive
+  userActivated.save()
+  // res.redirect("https://www.google.fr/")
+   res.send ({ success: true, message: "Account activated successfully"})
+  } catch (err) {
+  return res.status(404).send({ success: false, message: err })
+  }
+  })
+
+  /**
+ * as an admin i can disable or enable an account 
+ */
+  router.put('/status/edit',  async (req, res) =>  {
+    try {
+
+        let { idUser } = req.body
+        let user = await UserActivation.findOne({idUser}).select('+isActive')
+        user.isActive = !user.isActive
+        user.save()
+        res.status(200).send({ success: true, user })
+    } catch (err) {
+        return res.status(404).send({ success: false, message: err })
+    }
+   })
+
+
+  
+/*
+*************************************************************** 
+*/
+// fin traitement node mailer
 
 //Generate Token 
 const generateToken=(user) =>{
